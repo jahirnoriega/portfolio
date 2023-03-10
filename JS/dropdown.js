dbae03d60d173c5ac154f3910637ad8c6daa10e3
jahirnoriega <64813299+jahirnoriega@users.noreply.github.com>
2023-03-10 14:41:49 -0600

    Add files via upload

---
+++ JS/dropdown.js
@@ -0,0 +1,9 @@
+function showDropdown() {
+  var dropdown = document.getElementById("dropdown-content");
+  dropdown.classList.toggle("show");
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
